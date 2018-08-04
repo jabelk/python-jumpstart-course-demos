dac9a4b31c898ae86ea58549618be367e587c0b3
Jason Belk <jason.belk@networktocode.com>
2018-08-04 13:34:34 -0700

    app 1 complete

---
+++ apps/01_hello_world/you_try/app_1.py
@@ -0,0 +1,12 @@
+"""
+Python Jumpstart App 1
+"""
+__author__ = "Jason Belk"
+
+header = "-"*37
+print(header)
+space = " "*15
+print(space+"HELLO APP"+space)
+print(header)
+username = input("\n What is your name? ")
+print("Nice to meet you "+username)
